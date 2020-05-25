daf1829ee2aeddf78e298db825d1659c802240e7
mattcbodily <mattcbodily@gmail.com>
2020-05-25 10:42:48 -0600

    added loading animation to pokedex

--- src/Components/Pokedex/Pokedex.js
+++ src/Components/Pokedex/Pokedex.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react';
 import Header from '../Header/Header';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import pokeball from '../../assets/pokeball-loading.png';
 import './Pokedex.css';
 
 export default props => {
-    const [pokemonArr, setPokemonArr] = useState([]);
+    const [pokemonArr, setPokemonArr] = useState([]),
+          [loading, setLoading] = useState(true);
 
     const getAllPokemon = async() => {
         let allPokemon = [];
@@ -14,6 +16,7 @@ export default props => {
             allPokemon.push(pokemon.data);
         }
         setPokemonArr(allPokemon);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -22,21 +25,28 @@ export default props => {
 
     return (
         <div>
-            <Header />
-            <h1>Pokedex</h1>
-            <section className='pokedex-flex'>
-                {pokemonArr.map((pokemon, i) => (
-                    <Link to={`/pokemon/${pokemon.id}`} className={`pokemon-container ${pokemon.types.find(element => element.slot === 1).type.name}`} key={i}>
-                        <div className='pokemon-info'>
-                            <h4>{pokemon.name}</h4>
-                            {pokemon.types.sort((a, b) => a.slot - b.slot).map((type, i) => (
-                                <div key={i} className={`pokemon-type ${pokemon.types.find(element => element.slot === 1).type.name}-type`}>{type.type.name}</div>
-                            ))}
-                        </div>
-                        <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                    </Link>
-                ))}
-            </section>
+            {loading
+            ? (<>
+                <img src={pokeball} alt='loading' className='loading-image'/>
+                <h4 className='loading-prompt'>Loading...</h4>
+               </>)
+            : <>
+                <Header />
+                <h1>Pokedex</h1>
+                <section className='pokedex-flex'>
+                    {pokemonArr.map((pokemon, i) => (
+                        <Link to={`/pokemon/${pokemon.id}`} className={`pokemon-container ${pokemon.types.find(element => element.slot === 1).type.name}`} key={i}>
+                            <div className='pokemon-info'>
+                                <h4>{pokemon.name}</h4>
+                                {pokemon.types.sort((a, b) => a.slot - b.slot).map((type, i) => (
+                                    <div key={i} className={`pokemon-type ${pokemon.types.find(element => element.slot === 1).type.name}-type`}>{type.type.name}</div>
+                                ))}
+                            </div>
+                            <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+                        </Link>
+                    ))}
+                </section>
+              </>}
         </div>
     )
 };
