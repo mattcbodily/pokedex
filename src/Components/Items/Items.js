011f45f884de20cfc7cdf6f3e7b69d67b32f3c73
mattcbodily <mattcbodily@gmail.com>
2020-05-28 15:58:40 -0600

    added styling to the Items component

--- src/Components/Items/Items.js
+++ src/Components/Items/Items.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Header from '../Header/Header';
 import loading from '../../HOCs/loading';
+import grayIcon from '../../assets/gray-poke-svg.svg';
+import pokeIcon from '../../assets/pokesvg.svg';
 import pokeLoading from '../../assets/pokeball-loading.png';
 import './Items.css';
 
@@ -22,23 +24,24 @@ const Items = props => {
         getItems()
     }, [])
 
-    console.log(items)
-
     return (
-        <div>
+        <div className='items'>
             {props.loadingObj.loading
             ? (<>
                 <img src={pokeLoading} alt='loading' className='loading-image'/>
                 <h4 className='loading-prompt'>Loading...</h4>
                </>)
             : (<>
+                <img src={grayIcon} alt='large pokeball' className='gray-icon'/>
                 <Header />
                 <h1>Items</h1>
                 <div className='item-flex'>
                     {items.map((item, i) => (
-                        <div className='item'>
-                            <img src={item.sprites.default} alt={item.name}/>
+                        <div className='item' key={i}>
+                            <div className='item-bubble-style'></div>
+                            <img src={item.sprites.default} alt={item.name} className='item-image'/>
                             <h4>{item.name}</h4>
+                            <img src={pokeIcon} alt='Pokeball' className='item-background'/>
                         </div>
                     ))}
                 </div>
