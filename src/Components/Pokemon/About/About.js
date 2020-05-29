eadf991a9ec0577fa4b67c21723c040e37b15d84
mattcbodily <mattcbodily@gmail.com>
2020-05-29 15:28:37 -0600

    added box to display height and weight of Pokemon in About.js

--- src/Components/Pokemon/About/About.js
+++ src/Components/Pokemon/About/About.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './About.css';
 
-export default props => (
-    <div className='about'>
-        <p>{props.flavorText}</p>
-    </div>
-)
+export default props => {
+    let [height, setHeight] = useState(''),
+        [weight, setWeight] = useState(0);
+
+    const getHeight = () => {
+        let totalInches = Math.round(props.height * 3.94),
+            feet = Math.floor(totalInches / 12),
+            remInches = totalInches - (feet * 12);
+        
+        setHeight(`${feet}' ${remInches}"`);
+    }
+
+    useEffect(() => {
+        getHeight();
+    }, [])
+
+    console.log(height)
+
+    return (
+        <div className='about'>
+            <p>{props.flavorText}</p>
+            <section className='height-weight-box'>     
+            
+            </section>
+        </div>
+    )
+}
