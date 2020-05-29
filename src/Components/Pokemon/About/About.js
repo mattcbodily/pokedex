ca26044546d8c3fb7a9772dca26e91c1e8c8002b
mattcbodily <mattcbodily@gmail.com>
2020-05-29 15:53:10 -0600

    height and weight now display for pokemon from the About component

--- src/Components/Pokemon/About/About.js
+++ src/Components/Pokemon/About/About.js
@@ -3,12 +3,24 @@ import './About.css';
 
 export default props => {
     let [height, setHeight] = useState(''),
-        [weight, setWeight] = useState(0);
+        [metricHeight, setMetricHeight] = useState(''),
+        [weight, setWeight] = useState(0),
+        [metricWeight, setMetricWeight] = useState(0);
 
     const getHeight = () => {
         let totalInches = Math.round(props.height * 3.94),
             feet = Math.floor(totalInches / 12),
             remInches = totalInches - (feet * 12);
+
+        let centimeters = props.height * 10,
+            meters;
+
+        if(centimeters >= 100){
+            meters = centimeters / 100;
+            setMetricHeight(`${meters}m`);
+        } else {
+            setMetricHeight(`${centimeters}cm`);
+        }
         
         setHeight(`${feet}' ${remInches}"`);
     }
@@ -16,7 +28,11 @@ export default props => {
     const getWeight = () => {
         let pounds = props.weight / 4.536,
             trimNum = Number(pounds.toFixed(1));
+
+        let kilograms = props.weight / 10;
+            
         setWeight(trimNum);
+        setMetricWeight(kilograms);
     }
 
     useEffect(() => {
@@ -24,14 +40,18 @@ export default props => {
         getWeight();
     }, [])
 
-    console.log(height)
-    console.log(weight)
-
     return (
         <div className='about'>
             <p>{props.flavorText}</p>
             <section className='height-weight-box'>     
-            
+                <section>
+                    <p className='size-prompt'>Height</p>
+                    <p>{height} ({metricHeight})</p>
+                </section>
+                <section>
+                    <p className='size-prompt'>Weight</p>
+                    <p>{weight}lbs ({metricWeight}kg)</p>
+                </section>
             </section>
         </div>
     )
